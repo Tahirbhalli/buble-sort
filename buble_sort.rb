9d46b9023fccc4cb9f85bdd26a0ed625a88fb679
tahirbhalli <tahirbhalli16@gmail.com>
2020-03-05 13:13:17 +0500

    linter errors

--- buble_sort.rb
+++ buble_sort.rb
@@ -22,9 +22,9 @@ puts "\n"
 def bubble_sort_by(arr)
   (arr.length - 1).times do |i|
     (arr.length - i - 1).times do |j|
-      result = yield(arr[j],arr[j + 1])
+      result = yield(arr[j], arr[j + 1])
       next unless result. positive?
-      
+
       temp = arr[j]
       arr[j] = arr[j + 1]
       arr[j + 1] = temp
@@ -32,7 +32,7 @@ def bubble_sort_by(arr)
   end
   arr
 end
-arr2 = ['tahir', 'am', 'i']
+arr2 = %W['tahir', 'am', 'i']
 
 bubble_sort_by(arr2) do |left, right|
   left.length - right.length
